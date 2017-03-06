c1e722640e112f61cc8689eff3597579093f5d9b
Tboevil <sansroman@foxmail.com>
2017-03-06 15:42:51 +0800

    first version

---
+++ public/javascripts/event.js
@@ -0,0 +1,21 @@
+window.onload=function(){
+  var currentImg = 1;
+  var head_icon=document.getElementById("viewbox");
+  head_icon.addEventListener('click',function(e){
+     e = e || window.event;
+     target = e.target || e.srcElement;
+     var classN = target&&target.className.toUpperCase();
+     switch (classN) {
+       case 'PREV':
+         target.setAttribute('style','animation:disappear 1s')
+         classN = classN - 1||3;
+         document.getElementsByClassName('img-list')[classN].setAttribute('style','animation:appear 1s');
+         break;
+       case 'NEXT':
+         target.setAttribute('style','animation:disappear 1s')
+         classN = classN + 1 % 4||1;
+         document.getElementsByClassName('img-list')[classN].setAttribute('style','animation:appear 1s');
+         break;
+     }
+     },false);
+};
